d145fd74e74c958e06a26561645096e460bf9e69
hamdi-medamine <hamdimohamedamine60@gmail.com>
2021-03-30 00:45:16 +0100

    check point state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+
 import './App.css';
+import amine from './amine.jpg' ;
+import React, { Component } from 'react'
+import Profil from './Profil'
+ class App extends Component {
+  
+  state = {
+    Person: {
+      fullName: "Hamdi Med Amine",
+      bio: "student at go my code",
+      imgSrc: <img src={amine} alt=""></img>,
+      profession: "full stuck js",
+    },
+    show: false,
+    
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  };
+  
+
+  handleShowPerson = () => {
+    this.setState({
+      ...this.state,
+      show: !this.state.show
+    });
+  };
+
+  render() {
+    return (
+      <div className="centre">
+      
+        {this.state.show && (
+          <div>
+            <Profil/>
+            <h1>{this.state.Person.fullName}</h1>
+            <h1>{this.state.Person.bio}</h1>
+            <h1>{this.state.Person.profession}</h1>
+            <div>{this.state.Person.imgSrc}</div>
+            <br></br>
+          </div>
+        )}
+
+        <button className="btn" onClick={this.handleShowPerson  } >hide/show</button>
+      </div>
+    )
+  }
 }
-
-export default App;
+export default App
